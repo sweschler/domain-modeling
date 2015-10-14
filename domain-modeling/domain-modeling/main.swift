81745822bc2b87019751a7ecd0e278d0a623ab4f
Weschler, Sabrina <weschler@uw.edu>
2015-10-13 18:46:42 -0700

    Created the project.

---
+++ domain-modeling/domain-modeling/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  domain-modeling
+//
+//  Created by Weschler, Sabrina on 10/13/15.
+//  Copyright (c) 2015 Weschler, Sabrina. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
